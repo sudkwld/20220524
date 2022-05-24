30393b796d7a9a2478054f7dc230b5089a313c61
sudkwld <103632525+sudkwld@users.noreply.github.com>
2022-05-24 17:36:38 +0900

    Add files via upload

---
+++ jjanggu_html/js/product.js
@@ -0,0 +1,27 @@
+
+        $(document).ready(function () {
+
+
+
+            $("#productbox li:first-child").mouseover(function () {
+
+                $("#cimg").attr("src", "img/arugula2.png");
+
+            });
+
+            $("#productbox li:nth-child(2)").mouseover(function () {
+
+                $("#cimg").attr("src", "img/basil.png");
+
+            });
+
+
+            $("#productbox li:last-child").mouseover(function () {
+
+                $("#cimg").attr("src", "img/flower.png");
+
+            });
+
+
+
+        });
